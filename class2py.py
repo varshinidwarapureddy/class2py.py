c694199e266b3c24463c3bdf4fa4ac794e3bbd73
varshinidwarapureddy <73581376+varshinidwarapureddy@users.noreply.github.com>
2020-11-01 09:07:03 +0530

    Add files via upload

---
+++ class2py.py
@@ -0,0 +1,36 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> print(math.sqrt(10))
+3.1622776601683795
+>>> import math
+>>> print(math.sqrt(4))
+2.0
+>>> from random import*
+>>> print(randint(0,9))
+1
+>>> from random import*
+>>> print(randint(0,9))
+3
+>>> from random import*
+>>> print(randint(0,9),randint(0,9),randint(0,9))
+9 2 3
+>>> from random import*
+>>> print (randint(0,9),randint(0,9),randint(0,9),sep"")
+SyntaxError: invalid syntax
+>>> from random import*
+>>> print(randint(0,9),randint(0,9),randint(0,9),sep" ")
+SyntaxError: invalid syntax
+>>> from random import*
+>>> print(randint(0,9),randint(0,9),randint(0,9),sep="")
+612
+>>> from random import*
+>>> for i in range(10):
+print(randint(0,9),randint(0,9),randint(0,9),sep="")
+SyntaxError: expected an indented block
+>>> print(randint(0,9),randint(0,9),randint(0,9),sep="")
+528
+>>> 
+>>> 
+>>> 
+>>> e
